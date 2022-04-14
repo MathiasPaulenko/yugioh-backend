30c1a0a5ad4918975063684fe0ccfaae7057db3f
mathias <mathias.paulenko@gmail.com>
2022-04-14 14:59:54 +0200

    Model Card and elements implemented

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,34 @@
+
+9
+	README.md,8\e\8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+F
+yugioh-backend/asgi.py,7\6\76bed761ec7500524359be7a2ff301b1f1acf77e
+F
+yugioh-backend/wsgi.py,7\2\7284cf9a28f21a36feff0021cfed846f7b90c9ea
+:
+
+.gitignore,a\5\a5cc2925ca8258af241be7e5b0381edf30266302
+P
+ yugioh-backend/settings/local.py,d\4\d49c95a568ed6d38f9c2a76a8f5636d4be642b90
+9
+	manage.py,3\1\3156ad13e4d695cd526bbb7b031016ecba842270
+F
+yugioh-backend/urls.py,d\7\d75aa21ba5e9088bf88a258d50f3061749a465a8
+@
+requirements.txt,1\9\19359a61ae2446b51b549167b014da2fcf265768
+H
+apps/api/v1/card/apps.py,0\c\0cee61939403f327d353c5b31a15a5fcf37cae09
+I
+apps/api/v1/card/admin.py,d\8\d85faf9d5afe250fa68e5a2e81a2f17d54617f4a
+H
+apps/api/v1/base/apps.py,e\d\ed4656db4b2e47aa52c3e4a77dba153463f09c26
+O
+yugioh-backend/settings/base.py,3\2\324a8de4ff705e39a1ca4ce7d3a02fa5dbab1049
+G
+apps/api/v1/base/api.py,5\9\5949f33461fa7794de879a8e1320d068d8c87133
+L
+apps/api/v1/base/__init__.py,c\b\cb4c6e30fa9e126de3a0bccdcbdc50140113d9ed
+J
+apps/api/v1/base/models.py,1\3\13546123357c26cab32d8f58a9f8b89412c423ce
+K
+apps/api/v1/card/choices.py,e\9\e9a31be9e834fb9d089acbf707547fb745437973
--- apps/api/v1/card/models.py
+++ apps/api/v1/card/models.py
@@ -1,7 +1,111 @@
 from django.db import models
+from rest_framework import serializers
 
 from apps.api.v1.base.models import BaseModel
+from apps.api.v1.card import choices
+
+
+class Type(BaseModel):
+    type = serializers.ChoiceField(choices=choices.CARD_TYPE)
+
+    class Meta:
+        verbose_name = 'Type'
+        verbose_name_plural = 'Types'
+
+    def __str__(self):
+        return self.type
+
+
+class Subtype(BaseModel):
+    subtype = serializers.ChoiceField(choices=choices.CARD_SUBTYPE)
+
+    class Meta:
+        verbose_name = 'Subtype'
+        verbose_name_plural = 'Subtypes'
+
+    def __str__(self):
+        return self.subtype
+
+
+class Race(BaseModel):
+    race = serializers.ChoiceField(choices=choices.MONSTER_RACE)
+
+    class Meta:
+        verbose_name = 'Race'
+        verbose_name_plural = 'Races'
+
+    def __str__(self):
+        return self.race
+
+
+class Attribute(BaseModel):
+    attribute = serializers.ChoiceField(choices=choices.CARD_ATTRIBUTE)
+
+    class Meta:
+        verbose_name = 'Attribute'
+        verbose_name_plural = 'Attributes'
+
+    def __str__(self):
+        return self.attribute
+
+
+class Rarity(BaseModel):
+    rarity = serializers.ChoiceField(choices=choices.CARD_RARITY)
+
+    class Meta:
+        verbose_name = 'Rarity'
+        verbose_name_plural = 'Rarities'
+
+    def __str__(self):
+        return self.rarity
 
 
 class Card(BaseModel):
-    pass
+    serial_code = models.CharField('Serial Code', max_length=60, unique=True, blank=False, null=False)
+    card_number = models.CharField('Card Number', max_length=60, unique=False, blank=False, null=False)
+    name = models.CharField('Name', max_length=255, blank=False, null=False)
+    set_name = models.CharField('Set Name', max_length=255, blank=True, null=True)
+    edition = models.CharField("Edition", blank=True, max_length=80, default='')
+    amount = models.IntegerField('Amount', default=0)
+
+    rarity: Rarity = models.ForeignKey(Rarity, related_name='rarity', on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'Card'
+        verbose_name_plural = 'Cards'
+
+    @property
+    def subtype_name(self):
+        return self.rarity.rarity
+
+    def __str__(self):
+        return self.name
+
+
+class Monster(BaseModel):
+    description = models.CharField('Description', max_length=250, blank=False, null=False)
+    level = models.CharField("Level/Rank", max_length=20)
+    attack = models.CharField("Attack", max_length=20)
+    defence = models.CharField("Defence", max_length=20)
+    archetype = models.CharField('Archetype', max_length=250, blank=True, null=True)
+
+    subtype: Subtype = models.ForeignKey(Subtype, related_name='subtype', on_delete=models.CASCADE)
+    type: Type = models.ForeignKey(Type, related_name='type', on_delete=models.CASCADE)
+    race: Race = models.ForeignKey(Race, related_name='race', on_delete=models.CASCADE)
+    attribute: Attribute = models.ForeignKey(Attribute, related_name='attribute', on_delete=models.CASCADE)
+
+    @property
+    def subtype_name(self):
+        return self.subtype.subtype
+
+    @property
+    def type_name(self):
+        return self.type.type
+
+    @property
+    def race_name(self):
+        return self.race.race
+
+    @property
+    def attribute_name(self):
+        return self.attribute.attribute
