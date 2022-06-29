158967ecce8d919338f0f8ecdfa0663097e41959
mathias <mathias.paulenko@gmail.com>
2022-06-29 22:06:04 +0200

    base configuration added

--- apps/api/v1/base/pagination.py
+++ apps/api/v1/base/pagination.py
@@ -5,8 +5,8 @@ from rest_framework.response import Response
 class StandardResultsSetPagination(PageNumberPagination):
     page_query_param = "offset"  # this is the "page"
     page_size_query_param = "limit"  # this is the "page_size"
-    max_page_size = 1000
-    page_size = 100
+    max_page_size = 5000
+    page_size = 120
 
     def get_paginated_response(self, data):
         return Response(
@@ -14,6 +14,8 @@ class StandardResultsSetPagination(PageNumberPagination):
                 'count': self.page.paginator.count,
                 'next': self.get_next_link(),
                 'previous': self.get_previous_link(),
+                'page_size': self.page_size,
                 'data': data
+
             }
         )
